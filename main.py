e0858d3fe45e472c1266a865ea719be6789b159d
SaimNomani <muhammadsaimnomani951@gmail.com>
2025-11-17 23:38:39 +0500

    main.py updated

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 
 @app.get("/{context_id}")
 def get_policy(context_id: str):
-    return {"mam": 150.0, "asking_price": 200.0}
+    return [
+        {"mam": 150.0, "asking_price": 200.0},
+        {"mam": 140.0, "asking_price": 190.0},
+        {"mam": 130.0, "asking_price": 180.0},
+        {"mam": 120.0, "asking_price": 170.0},
+    ]
